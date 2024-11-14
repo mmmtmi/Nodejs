10430b21e8df2018dee1f45a24ab885b1422ae11
mmmtmi <minamii.tks@gmail.com>
2024-11-14 09:08:09 +0900

    add list2-2

---
+++ list2_2.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+var server = http.createServer(
+	(request,respose)=>{
+		respose.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
+	}
+);
+
+server.listen(3000);
