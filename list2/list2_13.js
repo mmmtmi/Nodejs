2c7800dc348704d3bfe7dc031bab1d9a1931fb76
mmmtmi <minamii.tks@gmail.com>
2024-11-21 22:06:27 +0900

    list2_13

--- list2/list2_13.js
+++ list2/list2_13.js
@@ -1,14 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
-const url =　require('url');
+const url = require('url');
 
-const index_page = http.createServer(getFromClient);
+
+
+const index_page = fs.readFileSync('./list2_9.ejs','utf8');
+const style_css = fs.readFileSync('./list2_11.css','utf8');
+
+var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
 
-function getFromClient(request , respopnse){
+function getFromClient(request , response){
 	var url_parts = url.parse(request.url);
 	switch (url_parts.pathname){
 		
@@ -17,14 +22,14 @@ function getFromClient(request , respopnse){
 			title: "Index",
 			content: "this is sample page of use template",
 		});
-		response.whiteHead(200, {'Content-Type': 'text/html'});
-		response.white(content);
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(content);
 		response.end();
 		break;
 		
-		case '/style.css':
+		case '/list2_11.css':
 		response.writeHead(200, {'Content-Type': 'text/css'});
-		response.Write(style_css);
+		response.write(style_css);
 		response.end();
 		break;
 		
