878696cd7f3734b6573cd7f05d022e97b9dc170d
mmmtmi <right1903@gmail.com>
2024-12-31 12:50:03 +0900

    copy /add

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -33,21 +33,40 @@ router.get('/', (req , res ,next) => {
 router.get('/add',(req, res, next) => {
     var data ={
         title: 'Hello/Add',
-        content: '新しいレコードを入力：'
+        content: '新しいレコードを入力：',
+        form: {name:'', mail:'' , age:0}
     }
     res.render('hello/add', data);
 });
-router.post('/add', (req,res,next ) => {
-    const nm = req.body.name;
-    const ml = req.body.mail;
-    const ag = req.body.age;
-    db.serialize(() => {
-        db.run('insert into mydata(name, mail, age) values(?, ?, ?)',
-            nm, ml, ag);
-    });
-    res.redirect('/hello');
+router.post('/add', [
+    check ('name' , 'NAME は必ず入力してください').notEmpty(),
+    check ('mail' , 'MAIL はメールアドレスを入力してください').isEmail(),
+    check ('age' , 'AGE は年齢（整数）を入力してください').isInt()
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        var result = '<ul class="text-danger">';
+        var result_arr = errors.array();
+        for (var n in result_arr){
+            result += '<li>' + result_arr[n].msg + '</li>'
+        }
+        result += '</ul>';
+        var data = {
+            title: 'Hello/Add',
+            content: result,
+            form: req.body
+        }
+        res.render('hello/add', data);
+    } else{
+        var nm = req.body.name;
+        var ml = req.body.mail;
+        var ag = req.body.age;
+        db.serialize(() => {
+            db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+        });
+        res.redirect('/hello'); 
+    }
 });
-
 router.get('/show', (req, res, next) => {
     const id =req.query.id;
     db.serialize(() => {
